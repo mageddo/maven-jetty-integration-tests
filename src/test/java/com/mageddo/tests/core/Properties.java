518f6cf191e7b7259cd4e3768991d588680d1c96
Elvis <edigitalb@gmail.com>
2015-07-03 01:21:11 -0300

    fixing Properties.java url

--- src/test/java/com/mageddo/tests/core/Properties.java
+++ src/test/java/com/mageddo/tests/core/Properties.java
@@ -6,6 +6,6 @@ package com.mageddo.tests.core;
  */
 public class Properties {
 	
-	public static final String BASE_PATH = "http://localhost:8181/jetty-junit-tests";
+	public static final String BASE_PATH = "http://localhost:8181/jetty-integration-tests";
 	
 }
